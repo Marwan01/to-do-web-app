023372004a256cfc11a339fb290a5bbccdb36d79
Mar <marwanest@live.fr>
2019-04-26 14:56:14 -0500

    connected to stitch
    and added gitignore

--- src/stitch/stitch.js
+++ src/stitch/stitch.js
@@ -1,16 +1,17 @@
 import { Stitch, RemoteMongoClient } from "mongodb-stitch-browser-sdk";
 
- const APP_ID = '<your-app-id>';
+ const APP_ID = 'to-do-ztwxx';
 
  // TODO:
  // 1. Initialize the app client
- // const app = 
+ const app = Stitch.initializeAppClient(APP_ID);
 
  // 2. Instatiate a RemoteMongoClient client and create a RemoteMongoDatabase object
  //    for the 'todo' collection.
- // const db = 
+ const db = app.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas")
+ .db('todo');
 
  // 3. Create a RemoteMongoCollection for the `items` collection.
- // let items = 
+ let items = db.collection("items");
 
  export {app, items};
